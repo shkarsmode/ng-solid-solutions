65152d04764f3d63e5e1231e44e47afa37b0ea9a
Shkarupa Daniil <zshkarrr@outlook.com>
2023-10-08 00:47:18 +0300

    #19 Rewrite interceptor with Observable

--- src/app/store/admin-auth-store/interceptors/admin-auth.interceptor.ts
+++ src/app/store/admin-auth-store/interceptors/admin-auth.interceptor.ts
@@ -1,28 +1,47 @@
 import {
+    HttpErrorResponse,
     HttpEvent,
     HttpHandler,
     HttpInterceptor,
     HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AdminAuthService } from '../services/admin-auth.service';
+import { Store, select } from '@ngrx/store';
+import { EMPTY, Observable, catchError, first, mergeMap } from 'rxjs';
+import { getAdminAuthAccessToken } from '../store/admin-auth.selectors';
 
 @Injectable()
 export class AdminAuthInterceptor implements HttpInterceptor {
-    constructor(private adminAuthService: AdminAuthService) {}
+    constructor(
+        private store$: Store
+    ) {}
 
     intercept(
         request: HttpRequest<unknown>,
         next: HttpHandler
     ): Observable<HttpEvent<unknown>> {
-        if (this.adminAuthService.accessToken) {
-            request = request.clone({
-                setHeaders: {
-                    'Authorization': `Bearer ${this.adminAuthService.accessToken}`
-                }
+        return this.store$.pipe(
+            select(getAdminAuthAccessToken),
+            first(),
+            mergeMap(token => {
+                const authRequest = token ? request.clone({
+                    setHeaders: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                }) : request;
+                return next.handle(authRequest).pipe(
+                    catchError(error => {
+                        if (error instanceof HttpErrorResponse) {
+                            if (error.status === 401) {
+                                console.log('redirect on login page or sign out')
+                                return EMPTY;
+                            }
+                        }
+                        
+                        throw error;
+                    })
+                )
             })
-        }
-        return next.handle(request);
+        )
     }
 }
--- src/app/store/admin-auth-store/services/admin-auth.service.ts
+++ src/app/store/admin-auth-store/services/admin-auth.service.ts
@@ -1,27 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { Store, select } from '@ngrx/store';
 import { Observable, map } from 'rxjs';
 import { BASE_PATH_URL } from 'src/shared/tokens/environment.tokens';
 import { AuthData } from '../store/admin-auth.reducer';
-import { getAdminAuthAccessToken } from '../store/admin-auth.selectors';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AdminAuthService {
-    public accessToken?: string;
-
     constructor(
         private http: HttpClient,
         private jwtHelperService: JwtHelperService,
-        private store$: Store,
         @Inject(BASE_PATH_URL) private basePath: string
-    ) {
-        this.store$.pipe(select(getAdminAuthAccessToken))
-            .subscribe(accessToken => this.accessToken = accessToken);
-    }
+    ) {}
 
     public login(body: { login: string, password: string }): Observable<AuthData> {
         return this.http.post<{ accessToken: string }>(
