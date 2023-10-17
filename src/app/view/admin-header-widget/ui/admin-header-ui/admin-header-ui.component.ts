d0a7ce2bf3521d4d3a8c5533ebc5d52c637d27e2
Shkarupa Daniil <zshkarrr@outlook.com>
2023-10-17 12:56:53 +0300

    #27 Add logout logic

--- src/app/view/admin-header-widget/ui/admin-header-ui/admin-header-ui.component.ts
+++ src/app/view/admin-header-widget/ui/admin-header-ui/admin-header-ui.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-header-ui',
-  templateUrl: './admin-header-ui.component.html',
-  styleUrls: ['./admin-header-ui.component.scss']
+    selector: 'app-admin-header-ui',
+    templateUrl: './admin-header-ui.component.html',
+    styleUrls: ['./admin-header-ui.component.scss'],
 })
 export class AdminHeaderUiComponent {
+    @Output() logout: EventEmitter<void> = new EventEmitter();
 
+    public onLogoutButtonClick(): void {
+        this.logout.emit();
+    }
 }
